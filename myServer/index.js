9a89de7cc5db4579e038c292ba360e71eec20d5e
khadar <khadarmohamed062@gmail.com>
2025-08-06 15:12:43 +0300

    Rename Routes to routes_temp

--- myServer/index.js
+++ myServer/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 const ContactRoutes = require('./routes/Contact');
 const OrderRoutes = require('./routes/order');
-const PaymentRoutes = require('./routes/payment'); // ✅ lowercase
+const PaymentRoutes = require('./routes/Payment'); // ✅ lowercase
 const BookingRoutes = require('./routes/Booking');
 const CheckoutRoutes = require('./routes/Checkout');
 const AuthRoutes = require('./routes/auth');
