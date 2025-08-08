abdbbb6b46964a15b434bc3939d71ca3a4542700
khadar <khadarmohamed062@gmail.com>
2025-08-08 18:12:51 +0300

    fix index.js routes/Order.js and temp_models/Order.js references to removed models

--- jama/frontendNew/myServer/routes/order.js
+++ jama/frontendNew/myServer/routes/order.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Order = require('../models/Order');
+const Order = require('../temp_models/Order');
+
 
 
 router.get('/', async (req, res) => {
