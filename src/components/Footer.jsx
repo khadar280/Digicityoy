97f5b15a6a19986f65df553afb8f0dc5ba4e0879
khadar <khadarmohamed062@gmail.com>
2025-08-27 19:17:05 +0300

    fixed Footer.jsx changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
-
-
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn, FaTiktok } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { FaInstagram, FaTiktok } from 'react-icons/fa';
 import './Footer.css';
 
 function Footer() {
@@ -21,22 +20,25 @@ function Footer() {
           <h4>{t('footer.contactTitle')}</h4>
           <p>{t('footer.email')}: info@digicity.fi</p>
           <p>{t('footer.phone')}: Boss:0453418323 </p>
-            <p>{t('footer.phone')}: Customer kannelmäki 0403614763</p>
-            <p>{t('footer.phone')}: Customer Puhos 0403614760</p>
+          <p>{t('footer.phone')}: Customer kannelmäki 0403614763</p>
+          <p>{t('footer.phone')}: Customer Puhos 0403614760</p>
           <p>{t('footer.address')}: Klaneettitie 12 Helsinki 00420</p>
         </div>
-<div className="footer-socials">
-  <h4>{t('footer.followUs')}</h4>
-  <div className="social-icons">
-    <a href="https://www.tiktok.com/@digicityoy" target="_blank" rel="noopener noreferrer"><FaTiktok /></a>
-    <a href="https://www.instagram.com/digicity21" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-  </div>
-</div>
 
+        <div className="footer-socials">
+          <h4>{t('footer.followUs')}</h4>
+          <div className="social-icons">
+            <a href="https://www.tiktok.com/@digicityoy" target="_blank" rel="noopener noreferrer"><FaTiktok /></a>
+            <a href="https://www.instagram.com/digicity21" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+          </div>
+        </div>
       </div>
 
       <div className="footer-bottom">
         <p>© {new Date().getFullYear()} DIGICITY. {t('footer.rights')}</p>
+        <Link to="/warranty" className="footer-link">
+          {t('footer.warranty')}
+        </Link>
       </div>
     </footer>
   );
