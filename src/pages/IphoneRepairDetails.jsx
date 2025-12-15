f33b930cdd40e8dfcb6b50cddeafad8374fece1b
khadar <khadarmohamed062@gmail.com>
2025-12-15 16:52:11 +0200

    IphoneRepairDetails.jsx and RepairDetailCard.jsx fixed

--- src/pages/IphoneRepairDetails.jsx
+++ src/pages/IphoneRepairDetails.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./IphoneRepairDetails.css";
 import iphoneImage from "../assets/iphone4.png";
-
 import seImage from "../assets/iphone3rd.png";
 import sImage from "../assets/iphone8.png";
 
@@ -9,54 +8,436 @@ import RepairDetailCard from "./RepairDetailCard";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
+
+const modernCamera = { BackCamera: 180, FrontCamera: 159, CAMERALENS: 89 };
+const midCamera = { BackCamera: 149, FrontCamera: 129, CAMERALENS: 69 };
+const oldCamera = { BackCamera: 49, FrontCamera: 49, CAMERALENS: 40 };
+
 export const iphoneModels = [
-  { model: "iPhone 16 Pro Max", screenRepair: 600, batteryReplacement: 149, backRepair: 199, image: iphoneImage },
-  { model: "iPhone 16 Pro", screenRepair: 549, batteryReplacement: 149, backRepair: 179, image: iphoneImage },
-  { model: "iPhone 16 Plus", screenRepair: 489, batteryReplacement: 149, backRepair: 159, image: iphoneImage },
-  { model: "iPhone 16", screenRepair: 469, batteryReplacement: 149, backRepair: 149, image: iphoneImage },
+ 
+  {
+    model: "iPhone 16 Pro Max",
+    screenRepair: 600,
+    batteryReplacement: 149,
+    backRepair: 199,
+    buttonsReplacement: 119,
+    chargePortRepair: 109,
+    housingRepair: 219,
+    ...modernCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 16 Pro",
+    screenRepair: 549,
+    batteryReplacement: 149,
+    backRepair: 179,
+    buttonsReplacement: 119,
+    chargePortRepair: 109,
+    housingRepair: 219,
+    ...modernCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 16 Plus",
+    screenRepair: 489,
+    batteryReplacement: 149,
+    backRepair: 159,
+    buttonsReplacement: 119,
+    chargePortRepair: 109,
+    housingRepair: 199,
+    ...modernCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 16",
+    screenRepair: 469,
+    batteryReplacement: 149,
+    backRepair: 149,
+    buttonsReplacement: 119,
+    chargePortRepair: 109,
+    housingRepair: 219,
+    ...modernCamera,
+    image: iphoneImage
+  },
 
-  // FIXED HERE ↓↓↓
-  { model: "iPhone 15 Pro Max", screenRepair: "195 / 399", batteryReplacement: 149, backRepair: 149, image: iphoneImage },
-  { model: "iPhone 15 Pro", screenRepair: "180 / 389", batteryReplacement: 139, backRepair: 149, image: iphoneImage },
-  { model: "iPhone 15 Plus", screenRepair: "139 / 349", batteryReplacement: 129, backRepair: 159, image: iphoneImage },
-  { model: "iPhone 15", screenRepair: "139 / 329", batteryReplacement: 99, backRepair: 130, image: iphoneImage },
+  // iPhone 15 Series
+  {
+    model: "iPhone 15 Pro Max",
+    screenRepair: "195 / 399",
+    batteryReplacement: 149,
+    backRepair: 149,
+    buttonsReplacement: 119,
+    chargePortRepair: 109,
+    housingRepair: 219,
+    ...modernCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 15 Pro",
+    screenRepair: "180 / 389",
+    batteryReplacement: 139,
+    backRepair: 149,
+    buttonsReplacement: 119,
+    chargePortRepair: 109,
+    housingRepair: 219,
+    ...modernCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 15 Plus",
+    screenRepair: "139 / 349",
+    batteryReplacement: 129,
+    backRepair: 159,
+    buttonsReplacement: 119,
+    chargePortRepair: 109,
+    housingRepair: 219,
+    ...modernCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 15",
+    screenRepair: "139 / 329",
+    batteryReplacement: 99,
+    backRepair: 130,
+    buttonsReplacement: 119,
+    chargePortRepair: 109,
+    housingRepair: 169,
+    ...modernCamera,
+    image: iphoneImage
+  },
 
-  { model: "iPhone 14 Pro Max", screenRepair: "159 / 339", batteryReplacement: 99, backRepair: 149, image: iphoneImage },
-  { model: "iPhone 14 pro", screenRepair: "159 / 319", batteryReplacement: 99, backRepair: 149, image: iphoneImage },
-  { model: "iPhone 14 Plus", screenRepair: "139 / 290", batteryReplacement: 99, backRepair: 149, image: iphoneImage },
-  { model: "iPhone 14", screenRepair: "139 / 280", batteryReplacement: 99, backRepair: 130, image: iphoneImage },
+  // iPhone 14 Series
+  {
+    model: "iPhone 14 Pro Max",
+    screenRepair: "159 / 339",
+    batteryReplacement: 99,
+    backRepair: 149,
+    buttonsReplacement: 109,
+    chargePortRepair: 119,
+    housingRepair: 249,
+    ...midCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 14 Pro",
+    screenRepair: "159 / 319",
+    batteryReplacement: 99,
+    backRepair: 149,
+    buttonsReplacement: 109,
+    chargePortRepair: 109,
+    housingRepair: 249,
+    ...midCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 14 Plus",
+    screenRepair: "139 / 290",
+    batteryReplacement: 99,
+    backRepair: 149,
+    buttonsReplacement: 109,
+    chargePortRepair: 109,
+    housingRepair: 199,
+    ...midCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 14",
+    screenRepair: "139 / 280",
+    batteryReplacement: 99,
+    backRepair: 130,
+    buttonsReplacement: 109,
+    chargePortRepair: 109,
+    housingRepair: 169,
+    ...midCamera,
+    image: iphoneImage
+  },
 
-  { model: "iPhone 13 Pro Max", screenRepair: "139 / 250", batteryReplacement: 79, backRepair: 149, image: iphoneImage },
-  { model: "iPhone 13 Pro", screenRepair: "129 / 230", batteryReplacement: 79, backRepair: 149, image: iphoneImage },
-  { model: "iPhone 13 mini", screenRepair: 199, batteryReplacement: 79, backRepair: 139, image: iphoneImage },
-  { model: "iPhone 13", screenRepair: "129 / 220", batteryReplacement: 79, backRepair: 139, image: iphoneImage },
+  // iPhone 13 Series
+  {
+    model: "iPhone 13 Pro Max",
+    screenRepair: "139 / 250",
+    batteryReplacement: 79,
+    backRepair: 149,
+    buttonsReplacement: 119,
+    chargePortRepair: 99,
+    housingRepair: 239,
+    ...midCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 13 Pro",
+    screenRepair: "129 / 230",
+    batteryReplacement: 79,
+    backRepair: 149,
+    buttonsReplacement: 119,
+    chargePortRepair: 99,
+    housingRepair: 239,
+    ...midCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 13",
+    screenRepair: "129 / 220",
+    batteryReplacement: 79,
+    backRepair: 139,
+    buttonsReplacement: 119,
+    chargePortRepair: 99,
+    housingRepair: 189,
+    ...midCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 13 mini",
+    screenRepair: 199,
+    batteryReplacement: 79,
+    backRepair: 139,
+    buttonsReplacement: 119,
+    chargePortRepair: 99,
+    housingRepair: 189,
+    ...midCamera,
+    image: iphoneImage
+  },
 
-  { model: "iPhone 12 Pro Max", screenRepair: "139 / 220", batteryReplacement: 79, backRepair: 139, image: iphoneImage },
-  { model: "iPhone 12 Pro", screenRepair: "129 / 200", batteryReplacement: 79, backRepair: 129, image: iphoneImage },
-  { model: "iPhone 12 mini", screenRepair: "129 / 200", batteryReplacement: 79, backRepair: 129, image: iphoneImage },
-  { model: "iPhone 12", screenRepair: "129 / 200", batteryReplacement: 79, backRepair: 129, image: iphoneImage },
+  // iPhone 12 Series
+  {
+    model: "iPhone 12 Pro Max",
+    screenRepair: "139 / 220",
+    batteryReplacement: 79,
+    backRepair: 139,
+    buttonsReplacement: 69,
+    chargePortRepair: 79,
+    housingRepair: 159,
+    ...midCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 12 Pro",
+    screenRepair: "129 / 200",
+    batteryReplacement: 79,
+    backRepair: 129,
+    buttonsReplacement: 69,
+    chargePortRepair: 79,
+    housingRepair: 159,
+    ...midCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 12 mini",
+    screenRepair: "129 / 200",
+    batteryReplacement: 79,
+    backRepair: 129,
+    buttonsReplacement: 69,
+    chargePortRepair: 79,
+    housingRepair: 159,
+    ...midCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 12",
+    screenRepair: "129 / 200",
+    batteryReplacement: 79,
+    backRepair: 129,
+    buttonsReplacement: 69,
+    chargePortRepair: 79,
+    housingRepair: 159,
+    ...midCamera,
+    image: iphoneImage
+  },
 
-  { model: "iPhone 11 Pro Max", screenRepair: "129 / 180", batteryReplacement: 79, backRepair: 129, image: iphoneImage },
-  { model: "iPhone 11 Pro", screenRepair: "119 / 150", batteryReplacement: 69, backRepair: 119, image: iphoneImage },
-  { model: "iPhone 11", screenRepair: "99 / 150", batteryReplacement: 60, backRepair: 119, image: iphoneImage },
+  // iPhone 11 Series
+  {
+    model: "iPhone 11 Pro Max",
+    screenRepair: "129 /180",
+    batteryReplacement: 79,
+    backRepair: 129,
+    buttonsReplacement: 99,
+    chargePortRepair: 79,
+    housingRepair: 149,
+    ...midCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 11 Pro",
+    screenRepair: "119 / 150",
+    batteryReplacement: 69,
+    backRepair: 119,
+    buttonsReplacement: 99,
+    chargePortRepair: 79,
+    housingRepair: 149,
+    ...midCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone 11",
+    screenRepair: "99 / 150",
+    batteryReplacement: 60,
+    backRepair: 119,
+    buttonsReplacement: 99,
+    chargePortRepair: 79,
+    housingRepair: 149,
+    ...midCamera,
+    image: iphoneImage
+  },
 
-  { model: "iPhone XS Max", screenRepair: "99 / 130", batteryReplacement: 60, backRepair: 119, image: iphoneImage },
-  { model: "iPhone XS", screenRepair: "90 / 129", batteryReplacement: 55, backRepair: 120, image: iphoneImage },
-  { model: "iPhone XR", screenRepair: "85 / 100", batteryReplacement: 55, backRepair: 100, image: iphoneImage },
-  { model: "iPhone X", screenRepair: "85 / 100", batteryReplacement: 55, backRepair: 100, image: iphoneImage },
+  // iPhone X Series
+  {
+    model: "iPhone XS Max",
+    screenRepair: "99 / 130",
+    batteryReplacement: 60,
+    backRepair: 119,
+    buttonsReplacement: 59,
+    chargePortRepair: 59,
+    housingRepair: 99,
+    ...oldCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone XS",
+    screenRepair: "90 / 129",
+    batteryReplacement: 55,
+    backRepair: 120,
+    buttonsReplacement: 59,
+    chargePortRepair: 59,
+    housingRepair: 99,
+    ...oldCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone XR",
+    screenRepair: "85 / 100",
+    batteryReplacement: 55,
+    backRepair: 100,
+    buttonsReplacement: 59,
+    chargePortRepair: 59,
+    housingRepair: 99,
+    ...oldCamera,
+    image: iphoneImage
+  },
+  {
+    model: "iPhone X",
+    screenRepair: "85 / 100",
+    batteryReplacement: 55,
+    backRepair: 100,
+    buttonsReplacement: 59,
+    chargePortRepair: 59,
+    housingRepair: 99,
+    ...oldCamera,
+    image: iphoneImage
+  },
 
-  { model: "iPhone SE(3rd gen)", screenRepair: 80, batteryReplacement: 55, backRepair: 90, image: seImage },
-  { model: "iPhone SE(2nd gen)", screenRepair: 75, batteryReplacement: 45, backRepair: 90, image: iphoneImage },
+  // iPhone 8 / 7 / 6 Series
+  {
+    model: "iPhone 8 Plus",
+    screenRepair: 80,
+    batteryReplacement: 50,
+    backRepair: 90,
+    buttonsReplacement: 49,
+    chargePortRepair: 59,
+    housingRepair: 89,
+    ...oldCamera,
+    image: sImage
+  },
+  {
+    model: "iPhone 8",
+    screenRepair: 80,
+    batteryReplacement: 50,
+    backRepair: 90,
+    buttonsReplacement: 49,
+    chargePortRepair: 59,
+    housingRepair: 89,
+    ...oldCamera,
+    image: sImage
+  },
+  {
+    model: "iPhone 7 Plus",
+    screenRepair: 70,
+    batteryReplacement: 45,
+    backRepair: 90,
+    buttonsReplacement: 59,
+    chargePortRepair: 59,
+    housingRepair: 89,
+    ...oldCamera,
+    image: sImage
+  },
+  {
+    model: "iPhone 7",
+    screenRepair: 70,
+    batteryReplacement: 40,
+    backRepair: 85,
+    buttonsReplacement: 39,
+    chargePortRepair: 39,
+    housingRepair: 85,
+    ...oldCamera,
+    image: sImage
+  },
+  {
+    model: "iPhone 6 Plus",
+    screenRepair: 50,
+    batteryReplacement: 40,
+    backRepair: 60,
+    buttonsReplacement: 39,
+    chargePortRepair: 49,
+    housingRepair: 99,
+    ...oldCamera,
+    image: sImage
+  },
+  {
+    model: "iPhone 6",
+    screenRepair: 50,
+    batteryReplacement: 40,
+    backRepair: 60,
+    buttonsReplacement: 39,
+    chargePortRepair: 49,
+    housingRepair: 99,
+    ...oldCamera,
+    image: sImage
+  },
+  {
+    model: "iPhone 6s Plus",
+    screenRepair: 50,
+    batteryReplacement: 40,
+    backRepair: 60,
+    buttonsReplacement: 39,
+    chargePortRepair: 49,
+    housingRepair: 99,
+    ...oldCamera,
+    image: sImage
+  },
+  {
+    model: "iPhone 6s",
+    screenRepair: 50,
+    batteryReplacement: 40,
+    backRepair: 60,
+    buttonsReplacement: 39,
+    chargePortRepair: 49,
+    housingRepair: 99,
+    ...oldCamera,
+    image: sImage
+  },
 
-  { model: "iPhone 8 Plus", screenRepair: 80, batteryReplacement: 50, backRepair: 90, image: sImage },
-  { model: "iPhone 8", screenRepair: 80, batteryReplacement: 50, backRepair: 90, image: sImage },
-
-  { model: "iPhone 7 Plus", screenRepair: 70, batteryReplacement: 45, backRepair: 90, image: sImage },
-  { model: "iPhone 7", screenRepair: 70, batteryReplacement: 40, backRepair: 85, image: sImage },
-
-  { model: "iPhone 6 Plus", screenRepair: 50, batteryReplacement: 40, backRepair: 60, image: sImage },
-  { model: "iPhone 6", screenRepair: 50, batteryReplacement: 40, backRepair: 60, image: sImage },
-  { model: "iPhone 6s", screenRepair: 60, batteryReplacement: 40, backRepair: 75, image: sImage }
+  // iPhone SE Series
+  {
+    model: "iPhone SE (3rd gen)",
+    screenRepair: 80,
+    batteryReplacement: 55,
+    backRepair: 90,
+    buttonsReplacement: 49,
+    chargePortRepair: 59,
+    housingRepair: 89,
+    ...oldCamera,
+    image: seImage
+  },
+  {
+    model: "iPhone SE (2nd gen)",
+    screenRepair: 75,
+    batteryReplacement: 45,
+    backRepair: 90,
+    buttonsReplacement: 49,
+    chargePortRepair: 59,
+    housingRepair: 89,
+    ...oldCamera,
+    image: seImage
+  }
 ];
 
 const IphoneRepairDetails = () => {
@@ -69,8 +450,9 @@ const IphoneRepairDetails = () => {
       {!selectedModel ? (
         <>
           <h2>{t("iphone.title")}</h2>
-          <div style={{ textAlign: "left", marginBottom: "1rem" }}></div>
-          <button className="back-btn" onClick={() => navigate("/")}>← {t("iphone.back")}</button>
+          <button className="back-btn" onClick={() => navigate("/")}>
+            ← {t("iphone.back")}
+          </button>
 
           <div className="iphone-grid">
             {iphoneModels.map((iphone, index) => (
@@ -91,6 +473,7 @@ const IphoneRepairDetails = () => {
           <button className="back-btn" onClick={() => setSelectedModel(null)}>
             ← {t("iphone.backToModels")}
           </button>
+
           <RepairDetailCard model={selectedModel.model} prices={selectedModel} />
         </div>
       )}
--- src/pages/RepairDetailCard.jsx
+++ src/pages/RepairDetailCard.jsx
@@ -1,103 +1,65 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { IoIosPhonePortrait } from "react-icons/io";
-import { IoPhonePortrait } from "react-icons/io5";
-import { useCart } from "../components/CartContext";
-import { MdBatteryCharging20 } from "react-icons/md";
-import { useTranslation } from "react-i18next";
+import React from "react";
 import "./RepairDetailCard.css";
 
 const RepairDetailCard = ({ model, prices }) => {
-  const navigate = useNavigate();
-  const { addToCart } = useCart();
-  const { t } = useTranslation();
-
-  const services = [
-    {
-      type: "screen",
-  
-      icon:<IoIosPhonePortrait />,
-      title: `${model} ${t("repair.screenTitle")}`,
-      price: prices.screenRepair,
-      description: t("repair.screenDesc"),
-      warranty: "12 months",
-      time: "1-3 Hours"
-    },
-    {
-      type: "battery",
-      icon: <MdBatteryCharging20 />,
-      title: `${model} ${t("repair.batteryTitle")}`,
-      price: prices.batteryReplacement,
-      description: t("repair.batteryDesc"),
-      warranty: "12 months",
-      time: "1-3 Hours"
-    },
-    {
-      type: "back",
-      icon: <IoPhonePortrait />,
-      title: `${model} ${t("repair.backTitle")}`,
-      price: prices.backRepair,
-      description: t("repair.backDesc"),
-      warranty: "12 months",
-      time: "1-3 Hours"
-    }
-  ];
-
-  const [selected, setSelected] = useState(services[0]);
-
-  const handleBook = () => {
-    addToCart({
-      name: selected.title,
-      price: `€${selected.price}`,
-      description: selected.description,
-      image: prices.image
-    });
-    navigate("/cart");
-  };
-
   return (
-    <div className="repair-detail-container">
-      <div className="tab-buttons">
-        {services.map((service) => (
-          <button
-            key={service.type}
-            className={`tab-btn ${selected.type === service.type ? "active" : ""}`}
-            onClick={() => setSelected(service)}
-          >
-            <span>{service.icon}</span> {t(`repair.${service.type}`)}
-          </button>
-        ))}
+    <div className="repair-details">
+      <h3>{model}</h3>
+
+      <div className="repair-item">
+        <span>Screen Repair</span>
+        <span>{prices.screenRepair} €</span>
       </div>
 
-      <div className="detail-card">
-        <div className="left">
-          <div className="icon">{selected.icon}</div>
-          <div className="price">€{selected.price}</div>
-          <button className="book-btn" onClick={handleBook}>
-            {t("repair.bookBtn")}
-          </button>
-        </div>
-        <div className="right">
-          <h3>{selected.title}</h3>
-          <p>{selected.description}</p>
-          <p><strong>{t("repair.warranty")}:</strong> <span className="highlight">{selected.warranty}</span></p>
-          <p><strong>{t("repair.time")}:</strong> <span className="highlight">{selected.time}</span></p>
-        </div>
+      <div className="repair-item">
+        <span>Battery Replacement</span>
+        <span>{prices.batteryReplacement} €</span>
       </div>
+
+      <div className="repair-item">
+        <span>Back Glass Repair</span>
+        <span>{prices.backRepair} €</span>
+      </div>
+
+      {prices.buttonsReplacement && (
+        <div className="repair-item">
+          <span>Buttons Replacement</span>
+          <span>{prices.buttonsReplacement} €</span>
+        </div>
+      )}
+
+      {prices.chargePortRepair && (
+        <div className="repair-item">
+          <span>Charging Port Repair</span>
+          <span>{prices.chargePortRepair} €</span>
+        </div>
+      )}
+
+      {prices.housingRepair && (
+        <div className="repair-item">
+          <span>Housing Replacement</span>
+          <span>{prices.housingRepair} €</span>
+        </div>
+      )}
+
+      {prices.BackCamera && (
+        <div className="repair-item">
+          <span>Back Camera Repair</span>
+          <span>{prices.BackCamera} €</span>
+        </div>
+      )}
+      {prices.FrontCamera && (
+        <div className="repair-item">
+          <span>Front Camera Repair</span>
+          <span>{prices.FrontCamera} €</span>
+        </div>
+      )}
+      {prices.CAMERALENS && (
+        <div className="repair-item">
+          <span>Camera Lens Repair</span>
+          <span>{prices.CAMERALENS} €</span>
+        </div>
+      )}
     </div>
   );
 };
