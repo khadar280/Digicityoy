b6e4369c479ca5d539ebf6bad8864a8d57b3d15d
khadar <khadarmohamed062@gmail.com>
2026-01-26 15:19:59 +0200

    update it bro myServer model/booking.js

--- myServer/routes/booking.js
+++ myServer/routes/booking.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Booking from "../models/Booking.js";
+import Booking from "../models/booking.js";
 import nodemailer from "nodemailer";
 
 const router = express.Router();
