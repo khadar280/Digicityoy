5f610ccca92e43f435e438c70fcdb4dc62453724
khadar <khadarmohamed062@gmail.com>
2026-02-10 12:47:37 +0200

    upddate en.json and fi.json and AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,9 +1,6 @@
-
-
-
 import React from "react";
 import "./AboutSection.css";
-import shakImage from "../assets/shak.jpg"
+import shakImage from "../assets/shak.jpg";
 import { useTranslation } from "react-i18next";
 
 const AboutSection = () => {
@@ -12,16 +9,16 @@ const AboutSection = () => {
   return (
     <section className="about-section">
       <div className="about-container">
-       
         <div className="about-image-wrapper">
-          <img
-            src={shakImage} alt ="About us"/> 
+          <img src={shakImage} alt="About Digicity" />
         </div>
+
         <div className="about-text">
           <h2>{t("about.title")}</h2>
           <p>{t("about.p1")}</p>
           <p>{t("about.p2")}</p>
           <p>{t("about.p3")}</p>
+
           <ul>
             <li>{t("about.list.one")}</li>
             <li>{t("about.list.two")}</li>
@@ -34,4 +31,3 @@ const AboutSection = () => {
 };
 
 export default AboutSection;
-
