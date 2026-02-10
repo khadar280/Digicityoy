f9ea2fdff32fb130906d75b9a9b894e46b6c872b
khadar <khadarmohamed062@gmail.com>
2026-02-10 14:52:25 +0200

    upddate Whychoose.jsx and Whychoose.css

--- src/components/Whychoose.jsx
+++ src/components/Whychoose.jsx
@@ -11,27 +11,10 @@ const Whychoose = () => {
   const features = [
   
    
-    {
-      icon: '✅',
-      title: t('why do we choose digicity'),
-      description: t('whychoose.warranty.description'),
-    },
+    
   ];
 
-  return (
-    <section className="why-choose">
-    
-      <div className="features">
-        {features.map((feature, index) => (
-          <div key={index} className="feature">
-            <span className="feature-icon">{feature.icon}</span>
-            <h3>{feature.title}</h3>
-            <p>{feature.description}</p>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+  
 };
 
 export default Whychoose;
