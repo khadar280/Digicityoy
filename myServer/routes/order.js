badfa7cd6a6be952625b47024584694c66aaf362
khadar <khadarmohamed062@gmail.com>
2026-02-15 16:35:10 +0200

    Fix case-sensitive path for models folder

--- myServer/routes/order.js
+++ myServer/routes/order.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Order = require('../Models/order.js');
+const Order = require('../models/order.js');
 
 router.get('/', async (req, res) => {
   try {
