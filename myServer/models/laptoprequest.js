401622649256f96190ed07c96be6fa4d498c179e
khadar <khadarmohamed062@gmail.com>
2026-02-26 11:15:49 +0200

    Fix  laptoprequest.js index.js models/laptoprequest.js

--- myServer/models/laptoprequest.js
+++ myServer/models/laptoprequest.js
@@ -1,12 +1,12 @@
-// models/laptopRequest.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const laptopRequestSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String, required: true },
   email: { type: String, required: true },
-  model: { type: String },
-  lang: { type: String, default: "en" },
+  model: { type: String, default: "N/A" },
+  lang: { type: String, enum: ["en", "fi"], default: "en" },
 }, { timestamps: true });
 
-module.exports = mongoose.models.LaptopRequest || mongoose.model('LaptopRequest', laptopRequestSchema);
+
+module.exports = mongoose.model("LaptopRequest", laptopRequestSchema, "laptop");
