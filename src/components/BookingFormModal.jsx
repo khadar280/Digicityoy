c048b45c5acc21fa2fbab6ca1121ba62f4538ea7
khadar <khadarmohamed062@gmail.com>
2026-03-05 14:06:55 +0200

    BookingFormModal.jsx

--- src/components/BookingFormModal.jsx
+++ src/components/BookingFormModal.jsx
@@ -58,7 +58,7 @@ const BookingFormModal = ({ service, onClose }) => {
 
     const fetchBookedTimes = async () => {
       try {
-        const res = await fetch(`${API_URL}/api/?date=${form.date.toISOString().split("T")[0]}`);
+        const res = await fetch(`${API_URL}/api/booking?date=${form.date.toISOString().split("T")[0]}`);
         if (!res.ok) throw new Error("Failed to fetch booked times");
 
         const data = await res.json();
